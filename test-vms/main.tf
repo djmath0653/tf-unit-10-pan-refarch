137b3539762e699dd4c3921be1523314c8ac03f1
Dan Mathias <danmathias@Dans-MacBook-Pro.local>
2019-04-23 16:55:39 -0600

    c

--- test-vms/main.tf
+++ test-vms/main.tf
@@ -15,28 +15,28 @@ resource "azurerm_resource_group" "testvmrg" {
 }
 
 ## get the id of web subnet
-data "azurerm_subnet" "sharedwebsubnet" {
+data "azurerm_subnet" "shared_web_subnet" {
   name                 = "${var.shared_web_subnet_name}"
   virtual_network_name = "${var.azure_refarch_vnet_name}"
   resource_group_name  = "${var.shared_resource_group_name}"
 }
 
 ## get the id of db subnet
-data "azurerm_subnet" "shareddbsubnet" {
+data "azurerm_subnet" "shared_db_subnet" {
   name                 = "${var.shared_db_subnet_name}"
   virtual_network_name = "${var.azure_refarch_vnet_name}"
   resource_group_name  = "${var.shared_resource_group_name}"
 }
 
 ## get the id of buisiness subnet
-data "azurerm_subnet" "sharedbusinesssubnet" {
+data "azurerm_subnet" "shared_business_subnet" {
   name                 = "${var.shared_web_subnet_name}"
   virtual_network_name = "${var.azure_refarch_vnet_name}"
   resource_group_name  = "${var.shared_resource_group_name}"
 }
 
 # Create the public ip for web test vm
-resource "azurerm_public_ip" "webtestpublicip" {
+resource "azurerm_public_ip" "web_test_publicip" {
   name                = "${var.web_test_publicip_name}"
   location            = "${var.testvm_resource_group_location}"
   resource_group_name = "${azurerm_resource_group.testvmrg.name}"
@@ -50,7 +50,7 @@ resource "azurerm_public_ip" "webtestpublicip" {
 }
 
 # Create the public ip for db test vm
-resource "azurerm_public_ip" "dbtestpublicip" {
+resource "azurerm_public_ip" "db_test_publicip" {
   name                = "${var.db_test_publicip_name}"
   location            = "${var.testvm_resource_group_location}"
   resource_group_name = "${azurerm_resource_group.testvmrg.name}"
@@ -64,7 +64,7 @@ resource "azurerm_public_ip" "dbtestpublicip" {
 }
 
 # Create the public ip for business
-resource "azurerm_public_ip" "businesstestpublicip" {
+resource "azurerm_public_ip" "business_test_publicip" {
   name                = "${var.business_test_publicip_name}"
   location            = "${var.testvm_resource_group_location}"
   resource_group_name = "${azurerm_resource_group.testvmrg.name}"
@@ -78,18 +78,18 @@ resource "azurerm_public_ip" "businesstestpublicip" {
 }
 
 ## Create network interface for web test
-resource "azurerm_network_interface" "webtestnic0" {
+resource "azurerm_network_interface" "web_test_nic0" {
   name                = "WebTestVM"
   resource_group_name = "${var.testvm_resource_group_name}"
   location            = "${var.testvm_resource_group_location}"
 
   ip_configuration {
     name                          = "web-test-nic0-ipconfig"
-    subnet_id                     = "${data.azurerm_subnet.sharedwebsubnet.id}"
+    subnet_id                     = "${data.azurerm_subnet.shared_web_subnet.id}"
     private_ip_address_allocation = "Dynamic"
 
     # private_ip_address            = "${var.panorama1_vnic0_private_ip}"
-    public_ip_address_id = "${azurerm_public_ip.webtestpublicip.id}"
+    public_ip_address_id = "${azurerm_public_ip.web_test_publicip.id}"
   }
 
   tags = {
@@ -98,18 +98,18 @@ resource "azurerm_network_interface" "webtestnic0" {
 }
 
 ## Create network interface for db test
-resource "azurerm_network_interface" "dbtestnic0" {
+resource "azurerm_network_interface" "db_test_nic0" {
   name                = "db-test-nic0-ipconfig"
   resource_group_name = "${var.testvm_resource_group_name}"
   location            = "${var.testvm_resource_group_location}"
 
   ip_configuration {
     name                          = "db-test-nic0-ipconfig"
-    subnet_id                     = "${data.azurerm_subnet.shareddbsubnet.id}"
+    subnet_id                     = "${data.azurerm_subnet.shared_db_subnet.id}"
     private_ip_address_allocation = "Dynamic"
 
     # private_ip_address            = "${var.panorama2_vnic0_private_ip}"
-    public_ip_address_id = "${azurerm_public_ip.dbtestpublicip.id}"
+    public_ip_address_id = "${azurerm_public_ip.db_test_publicip.id}"
   }
 
   tags = {
@@ -118,18 +118,18 @@ resource "azurerm_network_interface" "dbtestnic0" {
 }
 
 ## Create network interface for business test
-resource "azurerm_network_interface" "businesstestnic0" {
+resource "azurerm_network_interface" "business_test_nic0" {
   name                = "business-test-nic0-ipconfig"
   resource_group_name = "${var.testvm_resource_group_name}"
   location            = "${var.testvm_resource_group_location}"
 
   ip_configuration {
     name                          = "business-test-nic0-ipconfig"
-    subnet_id                     = "${data.azurerm_subnet.sharedbusinesssubnet.id}"
+    subnet_id                     = "${data.azurerm_subnet.shared_business_subnet.id}"
     private_ip_address_allocation = "Dynamic"
 
     # private_ip_address            = "${var.panorama2_vnic0_private_ip}"
-    public_ip_address_id = "${azurerm_public_ip.businesstestpublicip.id}"
+    public_ip_address_id = "${azurerm_public_ip.business_test_publicip.id}"
   }
 
   tags = {
@@ -139,11 +139,11 @@ resource "azurerm_network_interface" "businesstestnic0" {
 
 ## Create VMs
 # Web Test VM
-resource "azurerm_virtual_machine" "webtestvm" {
+resource "azurerm_virtual_machine" "web_test_vm" {
   name                          = "${var.web_test_1vm_name}"
   location                      = "${var.testvm_resource_group_location}"
   resource_group_name           = "${var.testvm_resource_group_name}"
-  network_interface_ids         = ["${azurerm_network_interface.webtestnic0.id}"]
+  network_interface_ids         = ["${azurerm_network_interface.web_test_nic0.id}"]
   vm_size                       = "Standard_DS1_v2"
   delete_os_disk_on_termination = true
 
@@ -177,11 +177,11 @@ resource "azurerm_virtual_machine" "webtestvm" {
 }
 
 # DB Test VM
-resource "azurerm_virtual_machine" "dbtestvm" {
+resource "azurerm_virtual_machine" "db_test_vm" {
   name                          = "${var.db_test_1vm_name}"
   location                      = "${var.testvm_resource_group_location}"
   resource_group_name           = "${var.testvm_resource_group_name}"
-  network_interface_ids         = ["${azurerm_network_interface.dbtestnic0.id}"]
+  network_interface_ids         = ["${azurerm_network_interface.db_test_nic0.id}"]
   vm_size                       = "Standard_DS1_v2"
   delete_os_disk_on_termination = true
 
@@ -215,11 +215,11 @@ resource "azurerm_virtual_machine" "dbtestvm" {
 }
 
 # Business Test VM
-resource "azurerm_virtual_machine" "businesstestvm" {
+resource "azurerm_virtual_machine" "business_test_vm" {
   name                          = "${var.business_test_1vm_name}"
   location                      = "${var.testvm_resource_group_location}"
   resource_group_name           = "${var.testvm_resource_group_name}"
-  network_interface_ids         = ["${azurerm_network_interface.businesstestnic0.id}"]
+  network_interface_ids         = ["${azurerm_network_interface.business_test_nic0.id}"]
   vm_size                       = "Standard_DS1_v2"
   delete_os_disk_on_termination = true
 
