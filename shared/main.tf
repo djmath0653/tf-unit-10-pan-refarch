213c785fa910a36ba0ba8aec7833b9a87c1b732b
Dan Mathias <danmathias@Dans-MacBook-Pro.local>
2019-04-24 09:10:31 -0600

    c

--- shared/main.tf
+++ shared/main.tf
@@ -679,3 +679,390 @@ resource "azurerm_lb_rule" "shared_vpn_all_ports" {
   probe_id                       = "${azurerm_lb_probe.internal_https_probe.id}"
   enable_floating_ip             = "True"
 }
+
+# Create the Firewall diagnostic storage account
+resource "azurerm_storage_account" "firewall_storage_acct" {
+  name                     = "${var.firewall_storage_acct_name}"
+  resource_group_name      = "${azurerm_resource_group.shared_resource_group.name}"
+  location                 = "${azurerm_resource_group.shared_resource_group.location}"
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "${var.environment_tag_name}"
+  }
+}
+
+# Create the Firewall availability set
+resource "azurerm_availability_set" "firewall_as" {
+  name                = "${var.shared_avail_set_name}"
+  resource_group_name = "${azurerm_resource_group.shared_resource_group.name}"
+  location            = "${azurerm_resource_group.shared_resource_group.location}"
+
+  tags = {
+    environment = "${var.environment_tag_name}"
+  }
+}
+
+# Create the public ip for Firewall 1
+resource "azurerm_public_ip" "firewall1_mgmt_publicip" {
+  name                = "${var.firewall1_mgmt_public_ip_name}"
+  resource_group_name = "${azurerm_resource_group.shared_resource_group.name}"
+  location            = "${azurerm_resource_group.shared_resource_group.location}"
+  sku                 = "Standard"
+  allocation_method   = "Static"
+  domain_name_label   = "${var.firewall1_mgmt_domain_name_label}"
+
+  tags = {
+    environment = "${var.environment_tag_name}"
+  }
+}
+
+# Create the public ip for Firewall 2
+resource "azurerm_public_ip" "firewall2_mgmt_publicip" {
+  name                = "${var.firewall2_mgmt_public_ip_name}"
+  resource_group_name = "${azurerm_resource_group.shared_resource_group.name}"
+  location            = "${azurerm_resource_group.shared_resource_group.location}"
+  sku                 = "Standard"
+  allocation_method   = "Static"
+  domain_name_label   = "${var.firewall2_mgmt_domain_name_label}"
+
+  tags = {
+    environment = "${var.environment_tag_name}"
+  }
+}
+
+# Create Firewalls os disks
+resource "azurerm_storage_account" "firewall1osdisk" {
+  name                     = "${join("", list(var.firewall1_os_disk_account_name, substr(md5(azurerm_resource_group.shared_resource_group.id), 0, 4)))}"
+  resource_group_name      = "${azurerm_resource_group.shared_resource_group.name}"
+  location                 = "${azurerm_resource_group.shared_resource_group.location}"
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+}
+
+resource "azurerm_storage_account" "firewall2osdisk" {
+  name                     = "${join("", list(var.firewall2_os_disk_account_name, substr(md5(azurerm_resource_group.shared_resource_group.id), 0, 4)))}"
+  resource_group_name      = "${azurerm_resource_group.shared_resource_group.name}"
+  location                 = "${azurerm_resource_group.shared_resource_group.location}"
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+}
+
+## Create network interfaces
+resource "azurerm_network_interface" "firewall1_nic0" {
+  name                = "${var.firewall1_vnic0_name}"
+  resource_group_name = "${azurerm_resource_group.shared_resource_group.name}"
+  location            = "${azurerm_resource_group.shared_resource_group.location}"
+
+  ip_configuration {
+    name                          = "firewall1-nic0-ipconfig"
+    subnet_id                     = "${data.azurerm_subnet.managment_subnet.id}"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "${var.firewall1_vnic0_private_ip}"
+    public_ip_address_id          = "${azurerm_public_ip.firewall1_mgmt_publicip.id}"
+  }
+
+  tags = {
+    environment = "${var.environment_tag_name}"
+  }
+}
+
+## Create network interfaces
+resource "azurerm_network_interface" "firewall1_nic1" {
+  name                = "${var.firewall1_vnic1_name}"
+  resource_group_name = "${azurerm_resource_group.shared_resource_group.name}"
+  location            = "${azurerm_resource_group.shared_resource_group.location}"
+
+  ip_configuration {
+    name                          = "firewall1-nic1-ipconfig"
+    subnet_id                     = "${data.azurerm_subnet.shared_public_subnet.id}"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "${var.firewall1_vnic1_private_ip}"
+
+    load_balancer_backend_address_pools_ids = ["${data.azurerm_lb_backend_address_pool.public_lb_backend_address_pool.id}",
+      "${data.azurerm_lb_backend_address_pool.internal_public_lb_backend_address_pool.id}",
+    ]
+  }
+
+  tags = {
+    environment = "${var.environment_tag_name}"
+  }
+}
+
+## Create network interfaces
+resource "azurerm_network_interface" "firewall1_nic2" {
+  name                = "${var.firewall1_vnic2_name}"
+  resource_group_name = "${azurerm_resource_group.shared_resource_group.name}"
+  location            = "${azurerm_resource_group.shared_resource_group.location}"
+
+  ip_configuration {
+    name                                    = "firewall1-nic2-ipconfig"
+    subnet_id                               = "${data.azurerm_subnet.shared_private_subnet.id}"
+    private_ip_address_allocation           = "Static"
+    private_ip_address                      = "${var.firewall1_vnic2_private_ip}"
+    load_balancer_backend_address_pools_ids = ["${data.azurerm_lb_backend_address_pool.internal_lb_backend_address_pool.id}"]
+  }
+
+  tags = {
+    environment = "${var.environment_tag_name}"
+  }
+}
+
+## Create network interfaces
+resource "azurerm_network_interface" "firewall1_nic3" {
+  name                = "${var.firewall1_vnic3_name}"
+  resource_group_name = "${azurerm_resource_group.shared_resource_group.name}"
+  location            = "${azurerm_resource_group.shared_resource_group.location}"
+
+  ip_configuration {
+    name                                    = "firewall1-nic3-ipconfig"
+    subnet_id                               = "${data.azurerm_subnet.shared_vpn_subnet.id}"
+    private_ip_address_allocation           = "Static"
+    private_ip_address                      = "${var.firewall1_vnic3_private_ip}"
+    load_balancer_backend_address_pools_ids = ["${data.azurerm_lb_backend_address_pool.vpn_lb_backend_address_pool.id}"]
+  }
+
+  tags = {
+    environment = "${var.environment_tag_name}"
+  }
+}
+
+## Create network interfaces
+resource "azurerm_network_interface" "firewall2_nic0" {
+  name                = "${var.firewall2_vnic0_name}"
+  resource_group_name = "${azurerm_resource_group.shared_resource_group.name}"
+  location            = "${azurerm_resource_group.shared_resource_group.location}"
+
+  ip_configuration {
+    name                          = "firewall2-nic0-ipconfig"
+    subnet_id                     = "${data.azurerm_subnet.managment_subnet.id}"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "${var.firewall2_vnic0_private_ip}"
+    public_ip_address_id          = "${azurerm_public_ip.firewall2_mgmt_publicip.id}"
+  }
+
+  tags = {
+    environment = "${var.environment_tag_name}"
+  }
+}
+
+## Create network interfaces
+resource "azurerm_network_interface" "firewall2_nic1" {
+  name                = "${var.firewall2_vnic1_name}"
+  resource_group_name = "${azurerm_resource_group.shared_resource_group.name}"
+  location            = "${azurerm_resource_group.shared_resource_group.location}"
+
+  ip_configuration {
+    name                          = "firewall2-nic1-ipconfig"
+    subnet_id                     = "${data.azurerm_subnet.shared_public_subnet.id}"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "${var.firewall2_vnic1_private_ip}"
+
+    load_balancer_backend_address_pools_ids = ["${data.azurerm_lb_backend_address_pool.public_lb_backend_address_pool.id}",
+      "${data.azurerm_lb_backend_address_pool.internal_public_lb_backend_address_pool.id}",
+    ]
+  }
+
+  tags = {
+    environment = "${var.environment_tag_name}"
+  }
+}
+
+## Create network interfaces
+resource "azurerm_network_interface" "firewall2_nic2" {
+  name                = "${var.firewall2_vnic2_name}"
+  resource_group_name = "${azurerm_resource_group.shared_resource_group.name}"
+  location            = "${azurerm_resource_group.shared_resource_group.location}"
+
+  ip_configuration {
+    name                                    = "firewall2-nic2-ipconfig"
+    subnet_id                               = "${data.azurerm_subnet.shared_private_subnet.id}"
+    private_ip_address_allocation           = "Static"
+    private_ip_address                      = "${var.firewall2_vnic2_private_ip}"
+    load_balancer_backend_address_pools_ids = ["${data.azurerm_lb_backend_address_pool.internal_lb_backend_address_pool.id}"]
+  }
+
+  tags = {
+    environment = "${var.environment_tag_name}"
+  }
+}
+
+## Create network interfaces
+resource "azurerm_network_interface" "firewall2_nic3" {
+  name                = "${var.firewall2_vnic3_name}"
+  resource_group_name = "${azurerm_resource_group.shared_resource_group.name}"
+  location            = "${azurerm_resource_group.shared_resource_group.location}"
+
+  ip_configuration {
+    name                                    = "firewall2-nic3-ipconfig"
+    subnet_id                               = "${data.azurerm_subnet.shared_vpn_subnet.id}"
+    private_ip_address_allocation           = "Static"
+    private_ip_address                      = "${var.firewall2_vnic3_private_ip}"
+    load_balancer_backend_address_pools_ids = ["${data.azurerm_lb_backend_address_pool.vpn_lb_backend_address_pool.id}"]
+  }
+
+  tags = {
+    environment = "${var.environment_tag_name}"
+  }
+}
+
+## Create Firewall VMs
+# Firewall 1
+resource "azurerm_virtual_machine" "firewall1_vm" {
+  name                          = "${var.firewall1_vm_name}"
+  resource_group_name           = "${azurerm_resource_group.shared_resource_group.name}"
+  location                      = "${azurerm_resource_group.shared_resource_group.location}"
+  vm_size                       = "${var.firewall_vm_size}"
+  availability_set_id           = "${azurerm_availability_set.firewall_as.id}"
+  delete_os_disk_on_termination = "true"
+
+  depends_on = ["azurerm_network_interface.firewall1_nic0",
+    "azurerm_network_interface.firewall1_nic1",
+    "azurerm_network_interface.firewall1_nic2",
+    "azurerm_network_interface.firewall1_nic3",
+  ]
+
+  plan {
+    name      = "${var.firewallSku}"
+    publisher = "${var.firewallPublisher}"
+    product   = "${var.firewallOffer}"
+  }
+
+  storage_image_reference {
+    publisher = "${var.firewallPublisher}"
+    offer     = "${var.firewallOffer}"
+    sku       = "${var.firewallSku}"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name          = "${join("", list(var.firewall1_vm_name, "-osDisk"))}"
+    vhd_uri       = "${azurerm_storage_account.firewall1osdisk.primary_blob_endpoint}vhds/${var.firewall1_vm_name}-${var.firewallOffer}-${var.firewallSku}.vhd"
+    caching       = "ReadWrite"
+    create_option = "FromImage"
+  }
+
+  os_profile {
+    computer_name  = "${var.firewall1_vm_name}"
+    admin_username = "${var.admin_username}"
+    admin_password = "${var.admin_password}"
+  }
+
+  primary_network_interface_id = "${azurerm_network_interface.firewall1_nic0.id}"
+
+  network_interface_ids = ["${azurerm_network_interface.firewall1_nic0.id}",
+    "${azurerm_network_interface.firewall1_nic1.id}",
+    "${azurerm_network_interface.firewall1_nic2.id}",
+    "${azurerm_network_interface.firewall1_nic3.id}",
+  ]
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
+# Firewall 2
+resource "azurerm_virtual_machine" "firewall2_vm" {
+  name                          = "${var.firewall2_vm_name}"
+  resource_group_name           = "${azurerm_resource_group.shared_resource_group.name}"
+  location                      = "${azurerm_resource_group.shared_resource_group.location}"
+  vm_size                       = "${var.firewall_vm_size}"
+  availability_set_id           = "${azurerm_availability_set.firewall_as.id}"
+  delete_os_disk_on_termination = "true"
+
+  depends_on = ["azurerm_network_interface.firewall2_nic0",
+    "azurerm_network_interface.firewall2_nic1",
+    "azurerm_network_interface.firewall2_nic2",
+    "azurerm_network_interface.firewall2_nic3",
+  ]
+
+  plan {
+    name      = "${var.firewallSku}"
+    publisher = "${var.firewallPublisher}"
+    product   = "${var.firewallOffer}"
+  }
+
+  storage_image_reference {
+    publisher = "${var.firewallPublisher}"
+    offer     = "${var.firewallOffer}"
+    sku       = "${var.firewallSku}"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name          = "${join("", list(var.firewall2_vm_name, "-osDisk"))}"
+    vhd_uri       = "${azurerm_storage_account.firewall2osdisk.primary_blob_endpoint}vhds/${var.firewall2_vm_name}-${var.firewallOffer}-${var.firewallSku}.vhd"
+    caching       = "ReadWrite"
+    create_option = "FromImage"
+  }
+
+  os_profile {
+    computer_name  = "${var.firewall2_vm_name}"
+    admin_username = "${var.admin_username}"
+    admin_password = "${var.admin_password}"
+  }
+
+  primary_network_interface_id = "${azurerm_network_interface.firewall2_nic0.id}"
+
+  network_interface_ids = ["${azurerm_network_interface.firewall2_nic0.id}",
+    "${azurerm_network_interface.firewall2_nic1.id}",
+    "${azurerm_network_interface.firewall2_nic2.id}",
+    "${azurerm_network_interface.firewall2_nic3.id}",
+  ]
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
+## public lb pool associations
+resource "azurerm_network_interface_backend_address_pool_association" "public_lb_fw1_address_pool_association" {
+  network_interface_id    = "${azurerm_network_interface.firewall1_nic1.id}"
+  ip_configuration_name   = "firewall1-nic1-ipconfig"
+  backend_address_pool_id = "${data.azurerm_lb_backend_address_pool.public_lb_backend_address_pool.id}"
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "public_lb_fw2_address_pool_association" {
+  network_interface_id    = "${azurerm_network_interface.firewall2_nic1.id}"
+  ip_configuration_name   = "firewall2-nic1-ipconfig"
+  backend_address_pool_id = "${data.azurerm_lb_backend_address_pool.public_lb_backend_address_pool.id}"
+}
+
+## private lb pool associations
+resource "azurerm_network_interface_backend_address_pool_association" "internal_lb_fw1_address_pool_association" {
+  network_interface_id    = "${azurerm_network_interface.firewall1_nic2.id}"
+  ip_configuration_name   = "firewall1-nic2-ipconfig"
+  backend_address_pool_id = "${data.azurerm_lb_backend_address_pool.internal_lb_backend_address_pool.id}"
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "internal_lb_fw2_address_pool_association" {
+  network_interface_id    = "${azurerm_network_interface.firewall2_nic2.id}"
+  ip_configuration_name   = "firewall2-nic2-ipconfig"
+  backend_address_pool_id = "${data.azurerm_lb_backend_address_pool.internal_lb_backend_address_pool.id}"
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "internal_public_lb_fw1_address_pool_association" {
+  network_interface_id    = "${azurerm_network_interface.firewall1_nic1.id}"
+  ip_configuration_name   = "firewall1-nic1-ipconfig"
+  backend_address_pool_id = "${data.azurerm_lb_backend_address_pool.public_lb_backend_address_pool.id}"
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "internal_public_lb_fw2_address_pool_association" {
+  network_interface_id    = "${azurerm_network_interface.firewall2_nic1.id}"
+  ip_configuration_name   = "firewall2-nic1-ipconfig"
+  backend_address_pool_id = "${data.azurerm_lb_backend_address_pool.public_lb_backend_address_pool.id}"
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "vpn_lb_fw1_address_pool_association" {
+  network_interface_id    = "${azurerm_network_interface.firewall1_nic3.id}"
+  ip_configuration_name   = "firewall1-nic3-ipconfig"
+  backend_address_pool_id = "${data.azurerm_lb_backend_address_pool.vpn_lb_backend_address_pool.id}"
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "vpn_lb_fw2_address_pool_association" {
+  network_interface_id    = "${azurerm_network_interface.firewall2_nic3.id}"
+  ip_configuration_name   = "firewall2-nic3-ipconfig"
+  backend_address_pool_id = "${data.azurerm_lb_backend_address_pool.vpn_lb_backend_address_pool.id}"
+}
--- shared/variables.tf
+++ shared/variables.tf
@@ -171,3 +171,141 @@ variable "internal_Public_lb_backend_pool_name" {
 variable "vpn_lb_backend_pool_name" {
   default = "Firewall-Layer-VPN"
 }
+
+## Firewall vars
+variable "admin_username" {
+  default = "xmrefarchadmin"
+}
+
+variable "admin_password" {
+  default = "s0wqLK0N5f0!"
+}
+
+variable "shared_avail_set_name" {
+  default = "Unit-10-PAN-RefArch-Firewall-AS"
+}
+
+variable "firewall_storage_acct_name" {
+  default = "unit10panrefarchv2shared"
+}
+
+variable "firewall_vm_size" {
+  description = "firewall VM Size"
+  default     = "Standard_DS3_v2"
+}
+
+variable "firewallSku" {
+  default = "bundle1"
+}
+
+variable "firewallOffer" {
+  default = "vmseries1"
+}
+
+variable "firewallPublisher" {
+  default = "paloaltonetworks"
+}
+
+variable "firewall1_mgmt_public_ip_name" {
+  default = "Unit-10-PAN-RefArch-firewall-1-PIP"
+}
+
+variable "firewall1_mgmt_domain_name_label" {
+  default = "unit-10-pan-refarch-firewall-1"
+}
+
+variable "firewall1_vm_name" {
+  default = "Unit-10-PAN-RefArch-Firewall-1"
+}
+
+variable "firewall1_vnic0_name" {
+  description = "firewall 1 Manamagement Interface"
+  default     = "azure-firewall1-VNIC0"
+}
+
+variable "firewall1_vnic0_private_ip" {
+  description = "firewall 1 Manamagement private ip"
+  default     = "10.255.255.6"
+}
+
+variable "firewall1_vnic1_name" {
+  description = "firewall 1 Public Interface"
+  default     = "azure-firewall1-VNIC1"
+}
+
+variable "firewall1_vnic1_private_ip" {
+  description = "firewall 1 Public private ip"
+  default     = "172.16.1.6"
+}
+
+variable "firewall1_vnic2_name" {
+  description = "firewall 1 Private Interface"
+  default     = "azure-firewall1-VNIC2"
+}
+
+variable "firewall1_vnic2_private_ip" {
+  description = "firewall 1 Public private ip"
+  default     = "10.5.0.6"
+}
+
+variable "firewall1_vnic3_name" {
+  description = "firewall 1 VPN Interface"
+  default     = "azure-firewall1-VNIC3"
+}
+
+variable "firewall1_vnic3_private_ip" {
+  description = "firewall 1 Public private ip"
+  default     = "10.5.15.6"
+}
+
+variable "firewall1_os_disk_account_name" {
+  default = "firewall1osdisk"
+}
+
+variable "firewall2_mgmt_public_ip_name" {
+  default = "Unit-10-PAN-RefArch-Firewall-2-PIP"
+}
+
+variable "firewall2_mgmt_domain_name_label" {
+  default = "unit-10-pan-refarch-firewall-2"
+}
+
+variable "firewall2_vm_name" {
+  default = "Unit-10-PAN-RefArch-Firewall-2"
+}
+
+variable "firewall2_vnic0_name" {
+  default = "azure-firewall2-VNIC0"
+}
+
+variable "firewall2_vnic0_private_ip" {
+  default = "10.255.255.7"
+}
+
+variable "firewall2_vnic1_name" {
+  default = "azure-firewall2-VNIC1"
+}
+
+variable "firewall2_vnic1_private_ip" {
+  default = "172.16.1.7"
+}
+
+variable "firewall2_vnic2_name" {
+  default = "azure-firewall2-VNIC2"
+}
+
+variable "firewall2_vnic2_private_ip" {
+  default = "10.5.0.7"
+}
+
+variable "firewall2_vnic3_name" {
+  default = "azure-firewall2-VNIC3"
+}
+
+variable "firewall2_vnic3_private_ip" {
+  default = "10.5.15.7"
+}
+
+variable "firewall2_os_disk_account_name" {
+  default = "firewall2osdisk"
+}
