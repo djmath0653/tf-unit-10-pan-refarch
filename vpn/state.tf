0b60359b58b10cced9af71f29eb6462e6b3dbf33
Dan Mathias <danmathias@Dans-MacBook-Pro.local>
2019-04-24 09:44:57 -0600

    c

--- vpn/state.tf
+++ vpn/state.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "unit10panrefarch"
     container_name       = "terraform-state"
-    key                  = "firewall.state"
+    key                  = "vpn.state"
     access_key           = "7N9tKzgmzyEEcndnD+yppUwDlCI65seqzaJ50dsFFlgSANbykDqevJbT3b/TQAG6uVMo3VXG0JwdoPknfQh2IA=="
   }
 }
