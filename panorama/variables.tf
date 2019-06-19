24f48e725852a6185e9e525aaadc014decc803d9
Dan Mathias <danmathias@Dans-MacBook-Pro.local>
2019-06-19 07:42:24 -0600

    c

--- panorama/variables.tf
+++ panorama/variables.tf
@@ -98,7 +98,7 @@ variable "panorama_avail_set_name" {
 }
 
 variable "panorama_storage_acct_name" {
-  default = "unit10panrefarchdiag-shared"
+  default = "unit10panrefarchdiagshared"
 }
 
 variable "panorama_vm_size" {
