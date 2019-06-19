ce3ddd70f0c0089e079829458452a5b08c7397a5
Dan Mathias <danmathias@Dans-MacBook-Pro.local>
2019-06-19 11:48:47 -0600

    change state names

--- test-vms/state.tf
+++ test-vms/state.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "unit10panrefarch"
     container_name       = "terraform-state"
-    key                  = "testvms.state"
+    key                  = "shared-testvms.state"
     access_key           = "7N9tKzgmzyEEcndnD+yppUwDlCI65seqzaJ50dsFFlgSANbykDqevJbT3b/TQAG6uVMo3VXG0JwdoPknfQh2IA=="
   }
 }
--- vpn/state.tf
+++ vpn/state.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "unit10panrefarch"
     container_name       = "terraform-state"
-    key                  = "vpn.state"
+    key                  = "shared-vpn.state"
     access_key           = "7N9tKzgmzyEEcndnD+yppUwDlCI65seqzaJ50dsFFlgSANbykDqevJbT3b/TQAG6uVMo3VXG0JwdoPknfQh2IA=="
   }
 }
