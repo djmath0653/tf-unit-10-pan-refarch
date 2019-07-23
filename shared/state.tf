572feb2fcc2c8be919d29f83d996aaa5325e0c1e
Dan <mathias.dan@gmail.com>
2019-07-23 12:40:45 -0600

    c

--- shared/state.tf
+++ shared/state.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "unit10panrefarch"
+    storage_account_name = "panaraterraform"
     container_name       = "terraform-state"
     key                  = "shared-shared.state"
-    access_key           = "7N9tKzgmzyEEcndnD+yppUwDlCI65seqzaJ50dsFFlgSANbykDqevJbT3b/TQAG6uVMo3VXG0JwdoPknfQh2IA=="
+    access_key           = "F4PssODHNxsGQvzQcmO5yK9Ixq8SKJsGeIFFR1c7JZWDTBQX+Aag3zX01SBVv7nt25C8pZGL5A5vT6xXj8I45Q=="
   }
 }
